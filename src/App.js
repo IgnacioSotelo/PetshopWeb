c2e7e16223658f1622680e2b10418392d1c47005
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2023-11-15 12:04:45 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import Item from './components/Item';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ItemConteiner from "./components/ItemContainer";
 import Background1 from './assets/background1.png';
+import Titulo from './components/Titulo';
 import "./App.css";
 
 function App() {
-  const [list, setList] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await Axios({
-          url: "https://api.mercadolibre.com/sites/MLA/search?seller_id=80348659",
-        });
+    return (
+        <div className="App">
+            <Navbar/>
 
-        setList(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+            <div className='wrapper-container'>
+                <div className='container'>
+                    <img className="img__background" src={Background1} alt='background' width={"100%"}></img>
+                </div>
+            </div>
 
-    fetchData();
 
-  }, [setList]);
+            <div className="wrapper-container">
+                <div className="container">
+                    <Titulo texto="Productos Destacados"/>
+                    
 
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className='wrapper-container'>
-        <div className='container'>
-          <img className="img__background"  src={Background1} alt='background' width={"100%"}></img>
+            
+                    <ItemConteiner destacados={true} />
+                    <Titulo texto="Producto para perros"/>
+                    <ItemConteiner destacados={false} filtro="perro" altura={655}/>
+                    <Titulo texto="Producto para Gatos"/>
+                    <ItemConteiner destacados={false} filtro="gato" altura={655}/>
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
-      </div>
-      <div className="wrapper-container">
-        <div className="container">
-          <ItemConteiner>
-            {
-              typeof (list.results) !== 'undefined' && list.results.map(item => {
-                return (<Item datos={item} key={item.id}></Item>)
-              })
-            }
-          </ItemConteiner>
-        </div>
-      </div>
-      <Footer></Footer>
-    </div>
-  );
+    );
 }
 
 export default App;
