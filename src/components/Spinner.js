184e365e5d1885f92563a5ed4d9f78837664f7b8
Ignacio Martin Sotelo <67351841+IgnacioSotelo@users.noreply.github.com>
2023-11-15 12:26:57 -0300

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ const Spinner = () => {
     return (
         <div className="div__spinner">
             <span className='span__spinner'>
-                <i class="fa-solid fa-spinner fa-spin"></i>
+                <i className="fa-solid fa-spinner fa-spin"></i>
             </span>
         </div>
     );
